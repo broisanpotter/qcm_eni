d329ef82df927cda3fbf272d43386f6ec0fcb2b7
broisanpotter <ambroise.rougier@gmail.com>
2018-09-12 14:12:14 +0200

    la maman des poissons elle est bien gentille

--- src/entity/Epreuve.java
+++ src/entity/Epreuve.java
@@ -1,19 +1,17 @@
 package entity;
 
-import java.util.Date;
-
 public class Epreuve {
 
-    Integer idEpreuve;
-    Date dateDebutValidite;
-    Date dateFinValidite;
-    Integer tempsEcoule;
-    String etat;
-    Double note_obtenue;
-    String niveau_obtenu;
-
-    Test test;
-    Utilisateur utilisateur;
-    ArrayList<QuestionTirage> questionsTirage;
+//    Integer idEpreuve;
+//    Date dateDebutValidite;
+//    Date dateFinValidite;
+//    Integer tempsEcoule;
+//    String etat;
+//    Double note_obtenue;
+//    String niveau_obtenu;
+//
+//    Test test;
+//    Utilisateur utilisateur;
+//    ArrayList<QuestionTirage> questionsTirage;
 
 }
