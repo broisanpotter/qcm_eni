7a9d93941425849faa82b2f7dfb58c7b8b9460d1
guiroc <gbonneau19@gmail.com>
2018-09-19 14:30:39 +0200

    gestion erreur creation epreuve

--- src/servlet/epreuve/ServletCreation.java
+++ src/servlet/epreuve/ServletCreation.java
@@ -19,97 +19,117 @@ import java.util.Date;
 public class ServletCreation extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        Date dateDebutValidite = null;
-        Date dateFinValidite = null;
-        Boolean formulaireNonCorrect = false;
-        Date date = new Date();
-        String message = "";
+        boolean checkSession = ConnectDB.checkSession(request, response);
 
-        SimpleDateFormat formatDate = new SimpleDateFormat("yy-MM-dd");
-        String[] utilisateursListe = request.getParameterValues("utilisateursListe");
-        Integer idTest = Integer.parseInt(request.getParameter("test"));
-        try {
-
-            dateDebutValidite = formatDate.parse(request.getParameter("dateDebutValidite"));
-            dateFinValidite = formatDate.parse(request.getParameter("dateFinValidite"));
-        } catch (ParseException e) {
-
-            e.printStackTrace();
+        if(!checkSession) {
+            response.sendRedirect("/login");
         }
-        if(utilisateursListe == null){
+        else {
+            Date dateDebutValidite = null;
+            Date dateFinValidite = null;
+            Boolean formulaireNonCorrect = false;
+            Date date = new Date();
+            String message = "";
 
-            formulaireNonCorrect = true;
-            message = message + "Pas d'utilsateur de selectionnée.";
-        }
-        if(dateFinValidite == null || dateDebutValidite == null){
+            SimpleDateFormat formatDate = new SimpleDateFormat("yy-MM-dd");
+            String[] utilisateursListe = request.getParameterValues("utilisateursListe");
+            Integer idTest = Integer.parseInt(request.getParameter("test"));
+            try {
 
-            formulaireNonCorrect = true;
-            if(message.length() > 0){message = message + "<br>";}
-            message = message + "Il manque une date.";
-        }
+                dateDebutValidite = formatDate.parse(request.getParameter("dateDebutValidite"));
+                dateFinValidite = formatDate.parse(request.getParameter("dateFinValidite"));
+            } catch (ParseException e) {
 
-        if(formulaireNonCorrect){
-
-            request.setAttribute("codeMessage", "error");
-            request.setAttribute("message", message);
-            request.setAttribute("dateDebutValidite", request.getParameter("dateDebutValidite"));
-            request.setAttribute("dateFinValidite", request.getParameter("dateFinValidite"));
-        }
-        else{
-
-            for (String idUtilisateur: utilisateursListe) {
-
-                Epreuve epreuve = new Epreuve();
-                epreuve.setDateDebutValidite(new java.sql.Date(dateDebutValidite.getTime()));
-                epreuve.setDateFinValidite(new java.sql.Date(dateFinValidite.getTime()));
-                epreuve.setEtat("EA");
-                epreuve.setIdTest(idTest);
-                epreuve.setIdUtilisateur(Integer.parseInt(idUtilisateur));
-
-                try {
-
-                    Integer idEpreuve = DalEpreuve.setEpreuve(epreuve);
-                    Test test = DalTest.getTest(idTest);
-                    ArrayList<Question> questionsListe = DalQuestion.getQuestionByTheme(test);
-                    SectionTest sectionTest = DalSectionTest.getSectionTest(test);
-                    ArrayList<Question> questionListSelectionnee = new ArrayList<Question>();
-                    ArrayList<Integer> nombresSorties = new ArrayList<Integer>();
-
-                    do{
-                        Integer random = (int)(Math.random()*(questionsListe.size()));
-                        if(!questionListSelectionnee.contains(questionsListe.get(random))){
-                            questionListSelectionnee.add(questionsListe.get(random));
-                        }
-                    }while (questionListSelectionnee.size() < sectionTest.getNbQuestionsATirer());
-
-                    Integer i = 1;
-                    for(Question question:questionListSelectionnee){
-
-                        QuestionTirage questionTirage = new QuestionTirage();
-                        questionTirage.setNumOrdre(i);
-                        questionTirage.setEstMarquee(false);
-                        questionTirage.setIdQuestion(question.getIdQuestion());
-                        questionTirage.setIdEpreuve(idEpreuve);
-                        DalQuestionTirage.setQuestionTirage(questionTirage);
-                        i++;
-                    }
-                } catch (SQLException e) {
-
-                    e.printStackTrace();
-                }
-
-
+                e.printStackTrace();
             }
-            request.setAttribute("codeMessage", "valid");
-            request.setAttribute("message", "Les modification ont été ajouter.");
-        }
+            if (utilisateursListe == null) {
 
-        Formulaire(request, response);
+                formulaireNonCorrect = true;
+                message = message + "Pas d'utilsateur de selectionnée.";
+            }
+            if (dateFinValidite == null || dateDebutValidite == null) {
+
+                formulaireNonCorrect = true;
+                if (message.length() > 0) {
+                    message = message + "<br>";
+                }
+                message = message + "Il manque une date.";
+            }
+
+            if (formulaireNonCorrect) {
+
+                request.setAttribute("codeMessage", "error");
+                request.setAttribute("message", message);
+                request.setAttribute("dateDebutValidite", request.getParameter("dateDebutValidite"));
+                request.setAttribute("dateFinValidite", request.getParameter("dateFinValidite"));
+            } else {
+
+                for (String idUtilisateur : utilisateursListe) {
+
+                    Epreuve epreuve = new Epreuve();
+                    epreuve.setDateDebutValidite(new java.sql.Date(dateDebutValidite.getTime()));
+                    epreuve.setDateFinValidite(new java.sql.Date(dateFinValidite.getTime()));
+                    epreuve.setEtat("EA");
+                    epreuve.setIdTest(idTest);
+                    epreuve.setIdUtilisateur(Integer.parseInt(idUtilisateur));
+
+                    try {
+
+                        Test test = DalTest.getTest(idTest);
+                        ArrayList<Question> questionsListe = DalQuestion.getQuestionByTheme(test);
+                        SectionTest sectionTest = DalSectionTest.getSectionTest(test);
+                        ArrayList<Question> questionListSelectionnee = new ArrayList<Question>();
+
+                        if (questionsListe.size() >= sectionTest.getNbQuestionsATirer()) {
+                            do {
+                                Integer random = (int) (Math.random() * (questionsListe.size()));
+                                if (!questionListSelectionnee.contains(questionsListe.get(random))) {
+                                    questionListSelectionnee.add(questionsListe.get(random));
+                                }
+                            } while (questionListSelectionnee.size() < sectionTest.getNbQuestionsATirer());
+
+
+                            Integer idEpreuve = DalEpreuve.setEpreuve(epreuve);
+                            Integer i = 1;
+                            for (Question question : questionListSelectionnee) {
+
+                                QuestionTirage questionTirage = new QuestionTirage();
+                                questionTirage.setNumOrdre(i);
+                                questionTirage.setEstMarquee(false);
+                                questionTirage.setIdQuestion(question.getIdQuestion());
+                                questionTirage.setIdEpreuve(idEpreuve);
+                                DalQuestionTirage.setQuestionTirage(questionTirage);
+                                i++;
+                            }
+                        } else {
+                            request.setAttribute("codeMessage", "error");
+                            request.setAttribute("message", "Il n'y a pas assez de question dans ce thème pour Crée cette épreuve.");
+                        }
+                        request.setAttribute("codeMessage", "valid");
+                        request.setAttribute("message", "Les modification ont été ajouter.");
+                    } catch (SQLException e) {
+
+                        request.setAttribute("codeMessage", "error");
+                        request.setAttribute("message", "Erreur SQL.");
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            Formulaire(request, response);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        Formulaire(request, response);
+        boolean checkSession = ConnectDB.checkSession(request, response);
+
+        if(!checkSession) {
+            response.sendRedirect("/login");
+        }
+        else {
+            Formulaire(request, response);
+        }
     }
 
     private void Formulaire(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
@@ -118,7 +138,7 @@ public class ServletCreation extends HttpServlet {
 
         try {
             testsListe = (ArrayList<Test>)DalTest.lister();
-            UtilisateursListe = (ArrayList<Utilisateur>)DalUser.listerUser(1);
+            UtilisateursListe = (ArrayList<Utilisateur>)DalUser.listerUser(2);
         } catch (SQLException e) {
             e.printStackTrace();
         }
